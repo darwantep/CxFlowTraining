64e727d8415264eb990b8c20a25e1b641594bace
darwantep <99869618+darwantep@users.noreply.github.com>
2023-03-31 17:49:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,21 @@ if __name__ == '__main__':
 if __name__ == '__main__':
  for arg in sys.argv[1:]:
  os.remove(arg)
-
+if __name__ == '__main__':
+ for arg in sys.argv[1:]:
+ os.remove(arg)
+ if __name__ == '__main__':
+ for arg in sys.argv[1:]:
+ os.remove(arg)
+ if __name__ == '__main__':
+ for arg in sys.argv[1:]:
+ os.remove(arg)
+ if __name__ == '__main__':
+ for arg in sys.argv[1:]:
+ os.remove(arg)
+ if __name__ == '__main__':
+ for arg in sys.argv[1:]:
+ os.remove(arg)
+ if __name__ == '__main__':
+ for arg in sys.argv[1:]:
+ os.remove(arg)
